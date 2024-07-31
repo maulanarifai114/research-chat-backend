f231e43b155ea2c1388919d8ad2df0d145b07d61
Raden Maulana Rifa'i Abdullah <maulanarifai114@gmail.com>
2024-07-31 23:49:15 +0700

    Setup realtime chat

---
+++ src/gateway/chat.gateway.ts
@@ -0,0 +1,29 @@
+import { SubscribeMessage, WebSocketGateway, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*', // Update this to match your frontend's origin
+  },
+})
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+
+  afterInit(server: Server) {
+    console.log('WebSocket initialized');
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log('Client connected:', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected:', client.id);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, payload: { sender: string; message: string }): void {
+    console.log('Payload', payload);
+    this.server.emit('message', payload);
+  }
+}
