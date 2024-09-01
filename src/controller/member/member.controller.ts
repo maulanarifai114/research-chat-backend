3dc6ef852f096c5f2176523cf207d91d9a7d31e0
firay24 <fisan.onodaconan@gmail.com>
2024-09-01 13:50:51 +0700

    feat: member by id user

--- src/controller/member/member.controller.ts
+++ src/controller/member/member.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { ConversationType } from '@prisma/client';
 import { Request } from 'express';
 import { Roles } from 'src/guard/roles/roles.decorator';
 import { Role } from 'src/guard/roles/roles.enum';
@@ -15,7 +16,7 @@ export class MemberController {
     private utilityService: UtilityService,
   ) {}
 
-  //#region List
+  //#region Conversation
   @Get('conversation/:idConversation')
   @Roles([Role.SUPERADMIN, Role.ADMIN, Role.MENTOR, Role.MEMBER])
   async getMemberByConversation(@Req() request: Request, @Param('idConversation') idConversation: string) {
@@ -53,7 +54,7 @@ export class MemberController {
     });
 
     const member: Member[] = dbMember.map((member) => ({
-      id: member.Id,
+      id: member.User.Id,
       name: member.User.Name,
       email: member.User.Email,
       role: member.User.Role,
@@ -65,6 +66,89 @@ export class MemberController {
         member,
       },
       message: 'Success Get List Member by ID Conversation',
+      statusCode: 200,
+    });
+  }
+  //#endregion
+
+  //#region User
+  @Get('user/:idUser')
+  @Roles([Role.SUPERADMIN, Role.ADMIN, Role.MENTOR, Role.MEMBER])
+  async getMemberByIdUser(@Req() request: Request, @Param('idUser') idUSer: string) {
+    const user = request.user;
+    const dbUser = await this.prismaService.user.findUnique({
+      where: { Id: user.id },
+    });
+
+    if (!dbUser) {
+      return this.utilityService.globalResponse({
+        statusCode: 400,
+        message: 'User not found',
+      });
+    }
+
+    const dbMember = await this.prismaService.member.findMany({
+      where: { IdUser: idUSer },
+      include: {
+        User: true,
+        Conversation: {
+          include: {
+            Member: {
+              include: {
+                User: true,
+              },
+              where: {
+                IdUser: {
+                  not: idUSer,
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const dbPrivateMember = dbMember.filter((member) => member.Conversation.Type == ConversationType.PRIVATE);
+    const privateMember: Member[] = dbPrivateMember.map((member) => ({
+      id: member.User.Id,
+      name: member.User.Name,
+      email: member.User.Email,
+      role: member.User.Role,
+    }));
+
+    const dbGroupMember = dbMember.filter((member) => member.Conversation.Type === ConversationType.GROUP);
+    const groupMember: Conversation[] = dbGroupMember.map((member) => ({
+      id: member.Conversation.Id,
+      name: member.Conversation.Name,
+      type: member.Conversation.Type,
+      member: member.Conversation.Member.map((member) => ({
+        id: member.User.Id,
+        name: member.User.Name,
+        email: member.User.Email,
+        role: member.User.Role,
+      })),
+    }));
+
+    const dbBroadcastMember = dbMember.filter((member) => member.Conversation.Type === ConversationType.BROADCAST);
+    const broadcastMember: Conversation[] = dbBroadcastMember.map((member) => ({
+      id: member.Conversation.Id,
+      name: member.Conversation.Name,
+      type: member.Conversation.Type,
+      member: member.Conversation.Member.filter((user) => user.User.Id !== idUSer).map((member) => ({
+        id: member.User.Id,
+        name: member.User.Name,
+        email: member.User.Email,
+        role: member.User.Role,
+      })),
+    }));
+
+    return this.utilityService.globalResponse({
+      data: {
+        privateMember,
+        groupMember,
+        broadcastMember,
+      },
+      message: 'Success Get List Member by ID User',
       statusCode: 200,
     });
   }
